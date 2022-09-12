ce8dc7952a09714fd4cd9277972e2c1dde21bec4
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-09-12 10:26:54 +0200

    lezione 2 - esercizio 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import "./training_typescript"
+import "./training_javascript"
+
+
 @NgModule({
   declarations: [
     AppComponent
--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,11 @@
-const nome = "romeo"
+const nome = "Renzo"
+const cognome = "Tramaglino"
+const nomeCognome = nome +" "+ cognome
+
+console.log(nomeCognome)
+
+const nomeCognome2 = `${nomeCognome}`
+
+console.log(nomeCognome2)
+
+
