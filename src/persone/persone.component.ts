a2cefbd3b9447c4a80783610176a9851a8b46562
hCri02 <c.hanu@itsrizzoli.it>
2022-09-12 17:11:36 +0200

    Components

---
+++ src/persone/persone.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+
+@Component({
+    selector:"persone",
+    templateUrl:"./persone.component.html",
+    styleUrls:["./persone.component.css"]
+})
+
+export class PersoneComponent {
+    nomePagina = "Persone Component"
+}
