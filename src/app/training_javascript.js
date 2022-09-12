38d2a07fccb37f0b38798c3a51222bdfbf94bbc6
hCri02 <c.hanu@itsrizzoli.it>
2022-09-12 10:30:52 +0200

    lezione2_esercizio1

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,9 @@
-const nome = "romeo"
+
+const nome = "renzo"
+const cognome = "tramaglino"
+
+const nomeCognome1 = nome + cognome
+const nomeCognome2 = `${nome}` + `${cognome}`
+
+console.log(nomeCognome1)
+console.log(nomeCognome2)
