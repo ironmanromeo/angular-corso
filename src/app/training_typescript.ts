ce6a2f429fd9946a0a1e1d9740ca5ca75c45e912
Gabriele <g.zulini@itsrizzoli.it>
2022-09-12 14:53:16 +0200

    lezione2_teoria classe

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,11 +1,25 @@
-class Persona {
+
+class Bipede {
+    piedi = 2
+}
+
+class Persona extends Bipede{
+
     nome:string
     cognome:string
     eta:number
 
     constructor(nome:string,cognome:string,eta:number) {
+        super()
         this.nome = nome
         this.cognome = cognome
         this.eta = eta 
     }
+
+    nomeCognome() {
+        return `Nome ${this.nome}, Cognome ${this.cognome} di anni ${this.eta}`
+    }
 }
+
+const persona1 = new Persona("Renzo","Tramaglino",20)
+const persona2 = new Persona("Lucia","Mondella",19)
