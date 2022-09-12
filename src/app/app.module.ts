9fcc7b81acda586fc54d630ee0294d243906449b
Rivi02 <r.verderio@itsrizzoli.it>
2022-09-12 10:30:07 +0200

    lezione 2 es 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import './training_javascript'
+import './training_typescript'
+
+
 @NgModule({
   declarations: [
     AppComponent
--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,6 @@
-const nome = "romeo"
+const nome = "Renzo"
+const cognome = "Tramaglino"
+const nomecognome= nome + cognome
+console.log (nomecognome)
+const nomecognome2 = `${nome} ${cognome}` 
+console.log (nomecognome2)
