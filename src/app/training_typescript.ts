7c486617ee8d8f724e180a83ed5defe1fdba3bc0
hCri02 <c.hanu@itsrizzoli.it>
2022-09-12 14:39:45 +0200

    Lezione 2 - teoria tipo

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,20 +1,30 @@
-const persone:any[] = []
 
-function creaPersona(nome:string, cognome:string, eta:number) {
-    const persona = {
-        nome:nome,
-        cognome:cognome,
-        eta:eta
-    }
-    return persona
+const eta = 32
+
+type Persona = {
+    nome:string,
+    cognome:string,
+    eta:number,
+    nomeCognome:any
 }
 
-const persona1 = creaPersona("Renzo","Tramaglino",20)
-const persona2 = creaPersona("Lucia","Mondella",19)
-const persona3 = creaPersona("Don","Abbondio",50)
+const persona1:Persona = {
+    nome: "Renzo",
+    cognome: "Ramaglino",
+    eta: 20,
+    nomeCognome(){
+        return this.nome + " " + this.cognome
+    }
+}
 
-persone.push(persona1)
-persone.push(persona2)
-persone.push(persona3)
+const persona2:Persona = {
+    nome: "Lucia",
+    cognome: "Mondella",
+    eta: 19,
+    nomeCognome(){
+        return this.nome + " " + this.cognome
+    }
+}
 
-persone.forEach(persona => console.log(persona))
+console.log(persona1.nomeCognome())
+console.log(persona2.nomeCognome())
