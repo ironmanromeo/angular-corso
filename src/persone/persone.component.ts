044113f8c106cae5e5dc13e2d933893abb6c5218
hCri02 <c.hanu@itsrizzoli.it>
2022-09-12 17:49:19 +0200

    Button

--- src/persone/persone.component.ts
+++ src/persone/persone.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core'
 
-
 @Component({
     selector:"persone",
     templateUrl:"./persone.component.html",
@@ -9,4 +8,17 @@ import { Component } from '@angular/core'
 
 export class PersoneComponent {
     nomePagina = "Persone Component"
+    disabilitato = true
+    contatore = 0
+    constructor() {
+        setTimeout(() => {
+            this.disabilitato = false
+        }, 1000)
+    }
+
+    incContatore() {
+        alert("azione bottone")
+        console.log(this.contatore++)
+    }
+
 }
