d7a41d6596c3d303914817e4dbcc548ee3e706c2
Marco Stefani <m.stefani@itsrizzoli.it>
2022-09-12 16:26:48 +0200

    feat: added library

---
+++ src/app/mia-libreria.ts
@@ -0,0 +1,57 @@
+export class Bipede{
+    piedi = 2
+}
+export class Persona extends Bipede {
+    nome: string
+    cognome: string
+    eta: number
+
+    constructor(nome: string, cognome: string, eta: number){
+        super()
+        this.nome = nome
+        this.cognome = cognome
+        this.eta = eta
+    }
+
+    nomeCognome(){
+        return `Nome: ${this.nome}, Cognome: ${this.cognome} di anni ${this.eta}`
+    }
+
+}
+
+export class Componente {
+    constructor(){
+        const ref = document.getElementById("componente")
+        if(ref){
+            ref.innerHTML = "<h5>Hello from Componente</h5>"
+        }
+    }
+}
+
+export class superPagina{
+    constructor(){
+        console.log("Hello from super pagina")
+    }
+
+    superMetodo(){
+        return "Cose meravigliose"
+    }
+}
+
+export function Logger(constructor: Function){
+    console.log("Decoratore logger")
+}
+
+type parametriDecoratore = {
+    selettore: string
+    template: string
+}
+
+export function ComponenteDecoratore(parametri: parametriDecoratore){
+    return function(constructor: Function){
+        const ref = document.getElementById(parametri.selettore)
+        if(ref){
+            ref.innerHTML = parametri.template
+        }
+    }
+}
