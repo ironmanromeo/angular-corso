4da0139d5d81610251237abd925499ad1f054fd2
Marco Stefani <m.stefani@itsrizzoli.it>
2022-09-12 12:30:34 +0200

    feat: esercizio 2

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,19 +1,27 @@
-let nomi: string[] = []
+let list: any[] = []
 
-function addnome(n: string){   
-    if(nomi.length<5){
-        nomi.push(n)        
-    }    
-    else{
-        console.log("Hai raggiunto il numero massimo di elementi inseribili")
-   }    
+function creapersona (nome: string, cognome: string, eta: number){
+    const persona = {
+        nome,
+        cognome,
+        eta
+    }
+
+    return persona
 }
 
-addnome("Marco")
-addnome("Francesco")
-addnome("Giovanni")
-addnome("Paolo")
-addnome("Omar")
-addnome("Ciao")
 
-console.log(nomi)
+
+
+const persona1 = creapersona("Renzo", "Tramaglino", 20)
+const persona2 = creapersona("Lucia", "Mondella", 19)
+const persona3 = creapersona("Don", "Abbondio", 50)
+
+list.push(persona1)
+list.push(persona2)
+list.push(persona3)
+
+
+list.forEach(persona => {
+    console.log(list)
+});
