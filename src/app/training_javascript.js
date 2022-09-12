64f8470fd93ab1056ed8460d8071409f9f5ef334
OmarBentaybi <o.bentaybi@itsrizzoli.it>
2022-09-12 10:29:42 +0200

    lezione 2 esercizio 1

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,7 @@
-const nome = "romeo"
+const nome= "Renzo"
+const cognome="Tramaglino"
+const nome_cognome= nome+" "+cognome
+console.log(nome_cognome)
+
+const nomeCognome2=`${nome} ${cognome}`
+console.log(nomeCognome2)
---
+++ src/app/training_typescript.ts
@@ -0,0 +1,9 @@
+const numerouno=28.5
+const numerodue= 5
+
+function somma(v1:number,v2:number){
+    return v1+v2
+}
+const risultato= somma(numerouno,numerodue)
+
+console.log(risultato)
