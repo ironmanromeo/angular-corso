db30d8f25a0b2654e8f18ee6efeb990558dcad2e
Marco Stefani <m.stefani@itsrizzoli.it>
2022-09-12 10:33:08 +0200

    fix: files

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,8 @@
-const nome = "romeo"
+const nome = "Renzo"
+const cognome = "Trimovoenf"
+
+const nomeCognome = nome + " " + cognome
+const nomeCognome2 = `${nome} ${cognome}`
+
+console.log(nomeCognome)
+console.log(nomeCognome2)
