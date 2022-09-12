bd84c72e57bfe0552c62623822c61a7b70c433f2
itscailor <l.cairo@itsrizzoli.it>
2022-09-12 10:29:28 +0200

    lezione2_esercizio1

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,6 @@
-const nome = "romeo"
+const nome = "renzo"
+const cognome = "Tramaglino"
+const nomecognome = nome+cognome
+console.log(nomecognome)
+const nomecognome2 = `${nomecognome}`
+console.log(nomecognome2)
---
+++ src/app/training_typescript.ts
@@ -0,0 +1,12 @@
+const numero1=28.5
+const numero2=5
+function somma(v1:number|string,v2:number|string){
+    if (typeof v1 === "number" && typeof v2 === "number"){
+        return v1 + v2
+    } else{
+        return 'Non sono numeri: v1 è ${v1}e v2 è ${v2}'
+    }
+}
+
+const risultato=somma(numero1,numero2)
+console.log(risultato)
