e18244bf666b703e1b8acbb584d643396cc02978
itscailor <l.cairo@itsrizzoli.it>
2022-09-12 10:43:43 +0200

    lezione2_esercizio1

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,6 +1,8 @@
 const nome = "renzo"
 const cognome = "Tramaglino"
 const nomecognome = nome+cognome
+console.log("normale:")
 console.log(nomecognome)
 const nomecognome2 = `${nomecognome}`
+console.log("Literals:")
 console.log(nomecognome2)
