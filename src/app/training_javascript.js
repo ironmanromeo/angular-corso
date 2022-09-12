31360b6cafbf36e17f638d6b9fed94421cb1feb3
Gabriele Callegari <g.callegari@itsrizzoli.it>
2022-09-12 10:29:32 +0200

    lezione 2 - es1

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,8 @@
-const nome = "romeo"
+const nome = "Renzo"
+const cognome = "Tramaglino"
+const nomeCognome = nome+" "+cognome
+console.log(nomeCognome)
+
+const nomeCognome2=`${nomeCognome}`
+console.log(nomeCognome2)
+
