f32c90e4cd696b91e6f369575e1b36e971eef5af
Rvbino <v.grasso@itsrizzoli.it>
2022-09-12 10:29:46 +0200

    lezione2-esercizio1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import './training_javascript'
+import './training_typescript'
+
 @NgModule({
   declarations: [
     AppComponent
@@ -16,3 +19,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,17 @@
-const nome = "romeo"
+/*const numero1 = 28.5
+const numero2 = "5"
+
+function somma(v1, v2){
+    return v1 + v2
+}
+
+const risultato = somma(numero1, numero2)
+
+console.log(risultato)*/
+
+const nome="romeo"
+const cognome="tramaglino"
+const nomecognome=nome+""+cognome
+console.log(nomecognome)
+const nomecognome2=`${nomecognome}`
+console.log(nomecognome2)
