fa4a318619a4967426d379e2e8bb0faa156f32d5
jimwellf <jimwellfulgencio@gmail.com>
2022-09-12 17:17:19 +0200

    teoria: use of decorator

---
+++ src/persone/persone.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"persone",
+    templateUrl:"./persone.component.html",
+    styleUrls:["./persone.component.css"]
+})
+export class PersoneComponent {
+    nomePagina = "Persone Component"
+}
