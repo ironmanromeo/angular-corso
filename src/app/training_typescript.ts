d602ec800084681bf7ff4c176ee1ba38009c1c8f
Marcaaa92 <78621850+Marcaaa92@users.noreply.github.com>
2022-09-12 14:38:32 +0200

    lezione2_esercizio4

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,10 +1,32 @@
-const nomi:string[] = []
 
-function creaPersona(nome:string, cognome:string, eta:number) {
-    const persona = {
-        nome:nome,
-        cognome:cognome,
-        eta:eta
-    }
-    return persona
+const eta = 32
+
+type persona = {
+    nome: string,
+    cognome: string,
+    eta: number,
+    nomeCognome: any
 }
+
+const persona1:persona= {
+    nome:"marca",
+    cognome:"marca",
+    eta:41,
+    nomeCognome()
+    {
+        return this.nome + " " + this.cognome
+    }
+}
+
+
+const persona2:persona = {
+    nome:"marca",
+    cognome:"marca",
+    eta:41,
+    nomeCognome()
+    {
+        return this.nome + " " + this.cognome
+    }
+}
+
+console.log(persona2.nomeCognome())
