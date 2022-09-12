1d10655af810da2b4ad6a8776b2c5f306815407f
Riccardo <c.lin@itsrizzoli.it>
2022-09-12 15:00:34 +0200

    lezione2_teoria classe

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,39 +1,26 @@
-type Persona ={
-    nome:string,
-    cognome:string,
-    eta:number,
-    nomeCognome:Function
+class Bipede{
+    piedi=2
 }
 
-// const persona1:{nome:string,cognome:string,eta?:number}= {
-//     nome:"Renzo",
-//     cognome:"Tramaglino",
-// }
-// const persona2:{nome:string,cognome:string,eta?:number}= {
-//     nome:"Lucia",
-//     cognome:"Mondella",
-//     eta:19
-// }
 
-const persona1:Persona= {
-    nome:"Renzo",
-    cognome:"Tramaglino",
-    eta:20,
+class Persona extends Bipede{
+
+    nome:string
+    cognome:string
+    eta:number
+
+    constructor(nome:string,cognome:string,eta:number){
+        super()
+        this.nome=nome
+        this.cognome=cognome
+        this.eta=eta
+    }
+
+
     nomeCognome(){
-        return this.nome+" "+this.cognome
+        return `Nome: ${this.nome}, Cognome: ${this.cognome}, di anni ${this.eta}`
     }
 }
 
-const persona2:Persona= {
-    nome:"Lucia",
-    cognome:"Mondella",
-    eta:19,
-    nomeCognome()
-    {
-        return this.nome+" "+this.cognome
-    }
-}
-
-console.log(persona1.nomeCognome());
-console.log(persona2.nomeCognome());
-
+const persona1=new Persona("Renzo","Tramaglino",20);
+const persona2=new Persona("Lucia","Mondella",19);
