8797fe2156f8867745962d5781c884144e04f2da
Gabriele <g.zulini@itsrizzoli.it>
2022-09-12 17:48:00 +0200

    lezione2_teoria finale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersoneComponent } from "../persone/persone.component"
 
 import './training_javascript'
 import './training_typescript'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersoneComponent
   ],
   imports: [
     BrowserModule,
