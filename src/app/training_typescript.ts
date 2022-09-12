b1d4a8f92dcae67cd26c84dc6cd458c0913a94af
Robert Hanu <r.hanu@itsrizzoli.it>
2022-09-12 12:30:01 +0200

    allineamento lezione2 - esercizio3

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,18 +1,23 @@
-const nomi:string[] = []
+const persone:any[] = [];
 
-function aggiungiNome(nome:string){
-    if(nomi.length < 5){
-        nomi.push(nome)
-    } else {
-        console.log("Errore...")
-    }
+function creaPersona(nome:string, cognome:string, eta:number):object {
+    const persona = {
+        nome: nome,
+        cognome: cognome,
+        eta: eta
+    };
+
+    return persona;
 }
 
-aggiungiNome("Nome1")
-aggiungiNome("Nome2")
-aggiungiNome("Nome3")
-aggiungiNome("Nome4")
-aggiungiNome("Nome5")
-aggiungiNome("Nome6")
+const persona1 = creaPersona("Renzo", "Tramaglino", 20);
+const persona2 = creaPersona("Lucia", "Mondella", 19);
+const persona3 = creaPersona("Don", "Abbondio", 50);
 
-console.log(nomi)
+persone.push(persona1)
+persone.push(persona2)
+persone.push(persona3)
+
+for(let i=0; i<persone.length; i++) {
+    console.log(persone[i]);
+}
