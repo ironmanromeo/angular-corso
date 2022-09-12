df0aa6535e9f742d213e94f353b6945ee8e5f7c3
Rvbino <v.grasso@itsrizzoli.it>
2022-09-12 17:09:40 +0200

    componente angular

---
+++ src/persone/persone.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector:"persone",
+    templateUrl:"./persone.component.html",
+    styleUrls:["./persone.component.css"]
+})
+
+export class PersoneComponent{
+    nomePagina = "Persone Component"    
+}
