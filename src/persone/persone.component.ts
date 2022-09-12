66c86605a709048c89d6111999c1f42821d7f534
Rivi02 <r.verderio@itsrizzoli.it>
2022-09-12 17:48:58 +0200

    lezione 2 bottone

--- src/persone/persone.component.ts
+++ src/persone/persone.component.ts
@@ -1,6 +1,5 @@
 import {Component} from "@angular/core"
 
-
 @Component({
     selector:"persone",
     templateUrl:"./persone.component.html",
@@ -8,4 +7,17 @@ import {Component} from "@angular/core"
 })
 export class PersoneComponent{
     nomePagina = "Persone Component"
+    disabilitato = true
+    contatore = 0
+    constructor(){
+        setTimeout(()=>{
+            this.disabilitato = false 
+        }, 1000)
+    }
+     
+    incContatore(){
+        this.contatore++
+        alert("azione bottone")
+    } 
+
 }
