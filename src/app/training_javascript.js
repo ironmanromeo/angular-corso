5b983a25c7f2c74274e9669c18bc98f4af945b08
Romeo Mariani <romeo.mariani@valxer.it>
2022-09-12 10:36:15 +0200

    lezione 2 - esercizio1 - soluzione/2

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,4 +1,6 @@
 const nome = "Renzo"
 const cognome = "Tramaglino"
 const nomeCognome = nome + " " + cognome
+console.log(nomeCognome)
 const nomeCognome2 = `${nome} ${cognome}`
+console.log(nomeCognome2)
