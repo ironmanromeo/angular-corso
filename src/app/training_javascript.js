fcf26104e39a4fec59bffc5d942e70d0900baa57
Robert Hanu <r.hanu@itsrizzoli.it>
2022-09-12 10:50:32 +0200

    allineamento lezione2 - esercizio1

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-const nome = 'Renzo';
-const cognome = 'Tramaglino';
-
-const nomeCognome = nome + ' ' + cognome;
-console.log(nomeCognome);
-
-const nomeCognome2 = `${nome} ${cognome}`;
-console.log(nomeCognome2);
-
-=======
 const nome = "Renzo"
 const cognome = "Tramaglino"
 const nomeCognome = nome + " " + cognome
 console.log(nomeCognome)
 const nomeCognome2 = `${nome} ${cognome}`
 console.log(nomeCognome2)
->>>>>>> origin/main
