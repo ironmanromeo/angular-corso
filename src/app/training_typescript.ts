ad2d46d1e6986e8620c9ed346bb1176798226af4
itscailor <l.cairo@itsrizzoli.it>
2022-09-12 14:38:22 +0200

    esercizio3

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -2,23 +2,25 @@ type persona={
     nome:string
     cognome:string
     eta:number
+    nomecognome:any
 }
-    
-    
     
     const persona1:persona= {
         nome: "Renzo",
         cognome: "Tramaglino",
-        eta:20
+        eta:20,
+        nomecognome(){
+            this.nome + " " +this.cognome
+        } 
     };
 
     const persona2:persona= {
         nome: "Renza",
         cognome: "Tramaglina",
-        eta:20
+        eta:23,
+        nomecognome(){
+            this.nome + " " +this.cognome
+        } 
     };
-
-console.log(persona1)
-console.log(Object.keys(persona1))
-console.log(persona2)
-console.log(Object.keys(persona2))
+console.log(persona1.nome,persona1.cognome)
+console.log(persona2.nome,persona2.cognome)
