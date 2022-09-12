ba0bd26815a6bb0796e567f59a53d069508f9a69
itscailor <l.cairo@itsrizzoli.it>
2022-09-12 14:28:19 +0200

    persone optional

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,14 +1,24 @@
-const persone:any[] = []
-function CreaPersona(nome:string,cognome:string,eta:any){
-    const persona ={
-        nome: nome,
-        cognome: cognome,
-        eta: eta
-    };
-    return persona
+type persona={
+    nome:string
+    cognome:string
+    eta:number
 }
-persone.push(CreaPersona("Renzo","Tramaglino",20))
-persone.push(CreaPersona("Lucia","Mondella",19))
-persone.push(CreaPersona("Don","Abbondio",50))
-persone.forEach(persona => console.log(persona))
+    
+    
+    
+    const persona1:persona= {
+        nome: "Renzo",
+        cognome: "Tramaglino",
+        eta:20
+    };
 
+    const persona2:persona= {
+        nome: "Renza",
+        cognome: "Tramaglina",
+        eta:20
+    };
+
+console.log(persona1)
+console.log(Object.keys(persona1))
+console.log(persona2)
+console.log(Object.keys(persona2))
