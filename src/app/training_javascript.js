dd0222c3d099431a65afb9e7f60cbe971f0f1204
Robert Hanu <r.hanu@itsrizzoli.it>
2022-09-12 10:29:56 +0200

    lezione 2 - esercizio 1

--- src/app/training_javascript.js
+++ src/app/training_javascript.js
@@ -1,1 +1,9 @@
-const nome = "romeo"
+const nome = 'Renzo';
+const cognome = 'Tramaglino';
+
+const nomeCognome = nome + ' ' + cognome;
+console.log(nomeCognome);
+
+const nomeCognome2 = `${nome} ${cognome}`;
+console.log(nomeCognome2);
+
