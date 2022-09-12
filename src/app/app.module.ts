f23a042347f11291788ff40e83bdceed9caa8e94
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-09-12 17:09:21 +0200

    lezione 2 - inizio angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PersoneComponent } from "../persone/persone.component"
 
 
 import './training_javascript'
@@ -10,7 +11,8 @@ import './training_typescript'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersoneComponent
   ],
   imports: [
     BrowserModule,
