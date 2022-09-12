e6e987c77625172aff9ab2e3003a8a08331a5f2c
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-09-12 11:59:05 +0200

    lezione 2 - allineamento esercizio 2

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,11 +1,18 @@
+const nomi:string[] = []
 
-//Esercizio 2
-
-const nomi: string[] = [] ;
-
-function aggiungiNome (nome:string){
-    if ( nomi.length > 5 ) console.log("errore...")
-    else (nomi.push(nome))
-
-    
+function aggiungiNome(nome:string){
+    if(nomi.length < 5){
+        nomi.push(nome)
+    } else {
+        console.log("Errore...")
+    }
 }
+
+aggiungiNome("Nome1")
+aggiungiNome("Nome2")
+aggiungiNome("Nome3")
+aggiungiNome("Nome4")
+aggiungiNome("Nome5")
+aggiungiNome("Nome6")
+
+console.log(nomi)
