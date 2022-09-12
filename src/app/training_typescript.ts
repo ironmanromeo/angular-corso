2ebafd22068b57ad80ab742b9c70cddd421d87d7
Gabriele <g.zulini@itsrizzoli.it>
2022-09-12 12:32:07 +0200

    allineamento_esercizio3

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,18 +1,28 @@
-const nomi:string[] = []
+const persone :any = []
 
-function aggiungiNome(nome:string){
-    if(nomi.length < 5){
-        nomi.push(nome)
-    } else {
-        console.log("Errore...")
+function creaPersona(nome:string,cognome:string,eta:number):Object{
+    const persona = {
+        nome,
+        cognome,
+        eta
     }
+    return persona;
 }
 
-aggiungiNome("Nome1")
-aggiungiNome("Nome2")
-aggiungiNome("Nome3")
-aggiungiNome("Nome4")
-aggiungiNome("Nome5")
-aggiungiNome("Nome6")
+function aggiungiNomi(persona:object){
+    persone.push(persona)
+}
 
-console.log(nomi)
+const persona1 = creaPersona("Renzo","Tramaglino"20)
+const persona2 = creaPersona("Lucia","Mondella",19)
+const persona3 = creaPersona("Don","Abbondio",50)
+
+persone.push(persona1)
+persone.push(persona2)
+persone.push(persona3)
+
+persone.forEach(persona => console.log(persona)){
+
+}
+    
+});
