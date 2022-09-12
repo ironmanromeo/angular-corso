f9f9b036aedf0b7e1c77d0d89da11d3361502e9c
riccardo de gasperi <riccardo.degasperi@itsrizzoli.it>
2022-09-12 11:32:39 +0200

    Lezione2 esercizio2 completo

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,13 +1,22 @@
 const nomi:string[] = []
 
-function riempi(arr:string[]) {
-    while (arr.length < 5) {
-        arr.push("UnNome")
+function aggiungiNome(arr:string[], nome:string) {
+
+    if (arr.length < 5) {
+        arr.push(nome)
+    } else {
+        console.log("Errore: array pieno")
     }
 
-console.log("Array pieno")
+    return nomi
 
-    return arr
 }
 
-riempi(nomi)
+aggiungiNome(nomi, "nome1")
+aggiungiNome(nomi, "nome2")
+aggiungiNome(nomi, "nome3")
+aggiungiNome(nomi, "nome4")
+aggiungiNome(nomi, "nome5")
+aggiungiNome(nomi, "nome6")
+
+console.log(nomi)
