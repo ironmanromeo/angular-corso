0f8d3b28157fedc0a762298447d15f489e0b1e16
Gabriele <g.zulini@itsrizzoli.it>
2022-09-12 11:54:28 +0200

    allineamento_esercizio2

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,14 +1,18 @@
-const nomi :string[]=[]
+const nomi:string[] = []
+
 function aggiungiNome(nome:string){
-    if(nomi.length<6){
+    if(nomi.length<5){
         nomi.push(nome)
     } else {
-        console.log("Massimo 5 nomi")
+        console.log("Errore...")
     }
+
     aggiungiNome("Marco")
     aggiungiNome("Paolo")
     aggiungiNome("Luca")
     aggiungiNome("Matteo")
     aggiungiNome("Ivan")
+    aggiungiNome("Ross")
+
     console.log(nomi)
 }
