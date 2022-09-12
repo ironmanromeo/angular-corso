facc297163da3449d5ff6bf245b32af9c3e8c840
Riccardo <c.lin@itsrizzoli.it>
2022-09-12 11:21:55 +0200

    lezione 2 esercizio 3

---
+++ src/app/training_typescript.ts
@@ -0,0 +1,19 @@
+const nomi: string[]= [];
+aggiungiNome();
+
+
+
+
+function aggiungiNome(){
+    if(nomi.length<5)
+    {
+        nomi.push("a");
+        nomi.push("a");    
+        nomi.push("a");
+        nomi.push("a");
+        nomi.push("a");
+
+    }
+    else
+    console.log("errore!!");
+}
