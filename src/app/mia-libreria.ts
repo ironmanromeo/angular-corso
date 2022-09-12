d3cafc543b50b82e86798c62b0eaf946b3828983
OmarBentaybi <o.bentaybi@itsrizzoli.it>
2022-09-12 16:13:23 +0200

    Teoria decoratori

---
+++ src/app/mia-libreria.ts
@@ -0,0 +1,63 @@
+export class Bipede{
+    piedi = 2
+}
+export class Persona extends Bipede{
+
+
+    nome:string
+    cognome:string
+    eta:number
+
+    constructor(nome:string,cognome:string,eta:number){
+        super()
+        this.nome = nome
+        this.cognome = cognome
+        this.eta = eta
+    }
+
+    nomeCognome(){
+        return `Nome: ${this.nome}, Cognome ${this.cognome} di anni ${this.eta}`
+    }
+
+}
+
+export class Componente {
+    constructor(){
+        const ref = document.getElementById("componente")
+        if (ref){
+            ref.innerHTML = "<h5>Hello from Componente</h5>"
+        }
+
+    }
+}
+
+//Fatta da me
+export class SuperPagina {
+    constructor(){
+        console.log("Hello from super pagina")
+    }
+    superMetodo(){
+        return "Cose meravigliose"
+    }
+}
+
+export function Logger(constructor:Function) {
+    
+}{
+    console.log("Decoratore logger")
+}
+
+type ParametriDecoratore = {
+    selettore:string
+    template:string 
+}
+
+export function ComponenteDecoratore(parametri: ParametriDecoratore){
+    return function (costruttor:Function){
+        
+            const ref = document.getElementById("componente")
+            if (ref){
+                ref.innerHTML = "<h5>Hello from Componentedecoratore</h5>"
+    }
+}
+}
--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,25 +1,10 @@
-class Bipede{
-    piedi = 2
-}
-class Persona extends Bipede{
+import { Componente,SuperPagina, Logger, ComponenteDecoratore } from './mia-libreria'
 
-
-    nome:string
-    cognome:string
-    eta:number
-
-    constructor(nome:string,cognome:string,eta:number){
-        super()
-        this.nome = nome
-        this.cognome = cognome
-        this.eta = eta
-    }
-
-    nomeCognome(){
-        return `Nome: ${this.nome}, Cognome ${this.cognome} di anni ${this.eta}`
-    }
+@ComponenteDecoratore({
+    selettore:"componente",
+    template:"<h1>Componente nuovo<h1>"
+})
+class Pagina extends SuperPagina{
 
 }
 
-const persona1 = new Persona("Renzo","Tramoglino",20)
-const persona2 = new Persona("Lucia","Mondella",19)
