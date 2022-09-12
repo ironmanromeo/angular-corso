998e447079ddf1722c665c2edbc45d1c718b419c
Rvbino <v.grasso@itsrizzoli.it>
2022-09-12 12:31:12 +0200

    lezione2-esercizio3

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,10 +1,20 @@
 const persona:any[] = []
 
 function crea(nome:string, cognome:string, eta:Number){
-    const umano={
+    const persona={
         nome: nome,
         cognome: cognome,
         eta: eta
     }
+    return persona
 }
 
+const persona1=crea("Renzo","Tramaglino",20)
+const persona2=crea("Lucia","Mondella",19)
+const persona3=crea("Don","Abbondio",50)
+
+persona.push(persona1)
+persona.push(persona2)
+persona.push(persona3)
+
+persona.forEach(persona =>console.log(persona))
