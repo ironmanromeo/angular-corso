78d40bbad09187e9d6389417d76320afab2e4139
Robert Hanu <r.hanu@itsrizzoli.it>
2022-09-12 14:53:33 +0200

    lezione2 - classi metodi oggetti

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,30 +1,26 @@
 
-const eta = 32
-
-type Persona = {
-    nome:string,
-    cognome:string,
-    eta:number,
-    nomeCognome:Function
+class Bipede {
+    piedi = 2
 }
 
-const persona1:Persona = {
-    nome: "Renzo",
-    cognome: "Tramaglino",
-    eta: 20,
-    nomeCognome() {
-        return this.nome + " " + this.cognome
+class Persona extends Bipede {
+
+    nome:string
+    cognome:string
+    eta:number
+
+    constructor(nome:string, cognome:string, eta:number) {
+        super()
+        this.nome = nome
+        this.cognome = cognome
+        this.eta = eta
     }
-}
 
-const persona2:Persona = {
-    nome: "Lucia",
-    cognome: "Mondella",
-    eta: 19,
     nomeCognome() {
-        return this.nome + " " + this.cognome
+        return `Nome: ${this.nome}, Cognome: ${this.cognome} di anni ${this.eta}`
     }
+
 }
 
-console.log(persona1.nomeCognome());
-console.log(persona2.nomeCognome());
+const persona1 = new Persona("Renzo", "Tramaglino", 20)
+const persona2 = new Persona("Lucia", "Mondella", 19)
