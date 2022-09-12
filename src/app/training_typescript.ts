86d5881fded1460701d89a9cafca9b0e0290f0df
Hicham <99076537+hicham-tijani@users.noreply.github.com>
2022-09-12 14:53:16 +0200

    allineamento_esercizio4

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,50 +1,17 @@
+class Persona{
 
-const eta = 32
+nome:string
+cognome:string
+eta:number
 
-type Persona = {
-    nome: string,
-    cognome: string,
-    eta: number,
-    nomeCognome:Function
+constructor(nome:string,cognome:string,eta:number){
+    this.nome=nome
+    this.cognome=cognome
+    this.eta=eta
 }
 
-const persona1:Persona = {
-    nome:"Renzo",
-    cognome:"Tramaglino",
-    eta:20,
-    nomeCognome(){
-        return this.nome + " " + this.cognome
-    }
+nomeCognome(){
+    return `nome:${this.nome},cognome:${this.cognome},eta:${this.eta}`
 }
 
-const persona2:Persona = {
-    nome:"Lucia",
-    cognome:"Mondella",
-    eta:19,
-    nomeCognome(){
-        return this.nome + " " + this.cognome
-    }
-    
-
-    const persona2:{nome:string,cognome:string,eta:number}= {
-        nome:"Lucia",
-        cognome:"Montella",
-        eta:19,
 }
-
-<<<<<<< HEAD
-console.log(persona2.nome,persona2.nome);
-
-=======
-const persona2:Persona = {
-    nome:"Lucia",
-    cognome:"Mondella",
-    eta:19,
-    nomeCognome(){
-        return this.nome + " " + this.cognome
-    }
-}
-
-console.log(persona1.nomeCognome())
-console.log(persona2.nomeCognome())
->>>>>>> origin/main
