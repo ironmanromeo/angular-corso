831c55131a54aab2ff7e2dac74206e254c3a9c31
Marco Stefani <m.stefani@itsrizzoli.it>
2022-09-15 15:15:47 +0200

    feat: modified ts

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,1 +1,15 @@
+const persona1 = {
+    nome: "Renzo",
+    cognome: "Tramaglimo",
+    eta: 20,
+    indirizzo: "Via B.Crespi",
+    citta: "Milano",
+    nazione: "Italia"
+}
 
+const { nome, cognome, eta, ...rest } = persona1
+
+const nuovoOggetto = { nome, cognome, eta }
+
+console.log("Qui oggetto persona1:", persona1)
+console.log("Qui oggetto rest:", rest)
