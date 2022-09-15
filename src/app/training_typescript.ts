720331ca59e15ab6d4aafd1b1fc8de162be693bf
Marcaaa92 <78621850+Marcaaa92@users.noreply.github.com>
2022-09-15 16:27:27 +0200

    LEZIONE3_ESERCIZIO3

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,17 +1,25 @@
-const datiAnagrafici = {
+//import { type } from "os"
+
+
+type Tipo1 = {
+    nome: string;
+    cognome: string;
+    eta: number
+  }
+  
+  type Tipo2 = {
+    altezza: number;
+    occhiali: boolean
+  }
+  
+  type PersonaTipo = Tipo1 & Tipo2
+  
+  const persona: PersonaTipo = {
     nome: "Renzo",
     cognome: "Tramaglino",
-    eta: 20
-}
-
-const datiBiometrici = {
-  pupilla: "chiara",
-  altezza: 1.74
-}
-
-const persona1 = { 
-  ...datiAnagrafici,
-  ...datiBiometrici
- }
-
- console.log("Qui persona1:", persona1)
+    eta: 40,
+    altezza: 1.70,
+    occhiali: false
+  }
+  
+  console.log(persona)
