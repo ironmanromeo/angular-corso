6bde807191b0744c765719f27f46c68e184f2903
riccardo de gasperi <riccardo.degasperi@itsrizzoli.it>
2022-09-15 16:26:31 +0200

    LEzione3 teoria4

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,18 +1,22 @@
+type Tipo1 = {
+    nome: string
+    cognome: string
+    eta: number
+}
 
+type Tipo2 = {
+    altezza: number
+    occhiali: boolean
+}
 
+type PersonaTipo = Tipo1 & Tipo2
 
-// union type
-type StringheNumeri = string | number
+const persona: PersonaTipo = {
+    nome: "Renzo",
+    cognome: "Tramaglino",
+    eta: 20,
+    altezza: 1.75,
+    occhiali: false 
+}
 
-function unisci(val1:StringheNumeri, val2:StringheNumeri) {
-    if (typeof val1 === "number" && typeof val2 === "number") {
-      return "Valore ritornato numero: " + (val1 + val2);
-    } else {
-      return "Valore ritornato stringa: " + val1 + val2;
-    }
-  }
-  
-  const risultato = unisci(12, 14);
-  
-  console.log(risultato);
-  
+console.log(persona)
