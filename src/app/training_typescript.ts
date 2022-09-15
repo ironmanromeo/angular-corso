6b46498c6cb6d5dbbdd3a3020f15e6652f6a0649
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-09-15 17:13:54 +0200

    lezione 3 - Allineamento  esercizio 6

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,25 +1,21 @@
-//import { type } from "os"
+const nomi: string[] = []
+const nomi2 = new Array<string>()
 
+nomi2.push("12")
 
-type Tipo1 = {
-    nome: string;
-    cognome: string;
-    eta: number
-  }
-  
-  type Tipo2 = {
-    altezza: number;
-    occhiali: boolean
-  }
-  
-  type PersonaTipo = Tipo1 & Tipo2
-  
-  const persona: PersonaTipo = {
-    nome: "Renzo",
-    cognome: "Tramaglino",
-    eta: 40,
-    altezza: 1.70,
-    occhiali: false
-  }
-  
-  console.log(persona)
+function pippo(param:string, param2:number) {
+
+}
+
+const pr = new Promise<string>((resolve, reject) => {
+  setTimeout(() =>{
+    resolve ("finito")
+
+  }, 3000)
+})
+
+console.log("Iniziato")
+pr.then (risposta => {
+    const lunghezzaStringa = risposta.length
+    console.log("Risposta:", risposta);
+})
