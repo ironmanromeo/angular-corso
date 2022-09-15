4c7b223751887ad0e9812c3eabff66628c092de8
Gabriele Callegari <g.callegari@itsrizzoli.it>
2022-09-15 16:26:51 +0200

    type union

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,3 +1,22 @@
-function unione(val1:number,val2:number){
-
+type Tipo1 = {
+    nome : string,
+    cognome : string,
+    eta : number
 }
+
+type Tipo2 = {
+    altezza : number,
+    occhiali : boolean
+}
+
+type PersonaTipo = Tipo1 & Tipo2
+
+const persona : PersonaTipo = {
+    nome:"Renzo",
+    cognome:"Tramaglino",
+    eta:20,
+    altezza:180,
+    occhiali:true
+}
+
+console.log(persona)
