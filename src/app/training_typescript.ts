753e6267fc44c8ddbae0ef064fa6a5c47666dc0c
riccardo de gasperi <riccardo.degasperi@itsrizzoli.it>
2022-09-15 17:51:35 +0200

    Lezione 3 teoria 5

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -7,10 +7,17 @@ const obj2 = {
   eta: 20
 }
 
-function merge<T, U>(val1: T, val2: U) {
+const obj3 = {
+  altezza: 1.74,
+  peso: "troppo"
+}
+
+function merge<T extends object, U extends object>(val1: T, val2: U) {
     return {...val1, ...val2}
 }
 
 const merged = merge(obj1, obj2)
+const merged2 = merge(obj1, obj3)
+// const merged3 = merge(merged, 23)
 
-console.log(merged.nome)
+console.log(merged)
