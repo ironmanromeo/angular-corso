5126a27caeb28a9378fa86878ebd3ff3de4c673e
Gabriele Callegari <g.callegari@itsrizzoli.it>
2022-09-15 17:14:53 +0200

    promise

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,25 +1,38 @@
-//import { type } from "os"
-
-
 type Tipo1 = {
     nome: string;
     cognome: string;
     eta: number
-  }
+}
 
-  type Tipo2 = {
-    altezza: number;
-    occhiali: boolean
-  }
+type Tipo2 = {
+altezza: number;
+occhiali: boolean
+}
 
-  type PersonaTipo = Tipo1 & Tipo2
+type PersonaTipo = Tipo1 & Tipo2
 
-  const persona: PersonaTipo = {
-    nome: "Renzo",
-    cognome: "Tramaglino",
-    eta: 40,
-    altezza: 1.70,
-    occhiali: false
-  }
+const persona: PersonaTipo = {
+nome: "Renzo",
+cognome: "Tramaglino",
+eta: 40,
+altezza: 1.70,
+occhiali: false
+}
 
-  console.log(persona)
+console.log(persona)
+
+
+const nomi: string[] =[]
+nomi.push("mario")
+const nomi2 = new Array<string>()
+
+const pr = new Promise<string>((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Finito")
+    },1000)
+})
+console.log("Iniziato")
+pr.then(risposta=>{
+    const lunghezza = risposta.length
+    console.log("Reply : "+lunghezza)
+})
