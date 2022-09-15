f67d8bd166f0bbf826293f40dd56934971bccd94
DaniAvolio <avoliodaniele9@gmail.com>
2022-09-15 15:27:13 +0200

    fix: prova allineamento

--- src/persone/persone.component.ts
+++ src/persone/persone.component.ts
@@ -1,31 +1,10 @@
-<<<<<<< HEAD
-import {Component} from "@angular/core"
-import { AppModule } from "src/app/app.module"
-=======
 import { Component } from "@angular/core"
->>>>>>> origin/main
 
 @Component({
     selector:"persone",
     templateUrl:"./persone.component.html",
     styleUrls:["./persone.component.css"]
 })
-<<<<<<< HEAD
-export class PersoneComponent{
-    nomePagina = "Persone Component"
-    disabilitato = true
-    contatore= 0
-    constructor(){
-        setTimeout(()=>{
-            this.disabilitato= false
-        },1000)
-    } 
-    incContatore(){
-        this.contatore++
-    }
-
-
-=======
 export class PersoneComponent {
 
     nomePagina = "Persone Component"
@@ -42,5 +21,4 @@ export class PersoneComponent {
         this.contatore++
     }
 
->>>>>>> origin/main
 }
