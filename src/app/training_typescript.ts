6c65d2f97d0a79cf700a47c66e7e1cc20b9fc90e
Gabriele Callegari <g.callegari@itsrizzoli.it>
2022-09-15 17:47:04 +0200

    merge

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,16 +1,22 @@
-const nomi: string[] = [];
-const nomi2 = new Array<string>();
+const obj1 = {
+    nome : "R",
+    cognome : "L"
+}
 
-nomi2.push("12");
+const obj2={
+    eta : 20
+}
 
-const pr = new Promise<string>((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Finito");
-  }, 10000);
-});
+const obj3 = {
+    altezza : 174,
+    peso : "ciao"
+}
 
-console.log("Iniziato");
-pr.then((risposta) => {
-  const lunghezzaStringa = risposta.length;
-  console.log("Risposta: ", risposta);
-});
+function merge<T,U>(v1:T,v2:U){
+    return Object.assign(v1,v2)
+}
+
+const merged = merge(obj1,obj2)
+const merged2= merge(obj1,obj3)
+
+console.log(merged2)
