5995d7d3ca3de3b6a159dcdf992101301e1624b9
Romeo Mariani <romeo.mariani@valxer.it>
2022-09-19 16:28:57 +0200

    Lezione 4 - Esercizio 5 - Property binding

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -13,7 +13,6 @@ export class PostsComponent implements OnInit {
 
   titolo = "Mio titolo"
   disabilitato = false
-  testoStatoBottone = ""
 
   constructor() { }
 
@@ -21,29 +20,19 @@ export class PostsComponent implements OnInit {
   }
 
   onPremuto() {
-    const t = prompt("Inserisci titolo", this.titolo)
-    if (t){
-      this.titolo = t
-    } else {
-      this.titolo = ""
+    const nomePost = prompt("Inserisci nuovo post")
+    if (nomePost) {
+      const newPost:Post = {
+        titolo:nomePost,
+        testo:"Test",
+        commenti:[]
+      }
+      this.posts.push(newPost)
     }
   }
 
-  onLoading() {
-    this.disabilitato = true
-    this.testoStatoBottone = "loading..."
-
-    setTimeout(()=> {
-      this.disabilitato = false
-      this.testoStatoBottone = ""
-    },2000)
+  getPostsString() {
+    return JSON.stringify(this.posts)
   }
-
-
-
-  // 1) bottone Accendi: premendo si deve disabilitare
-  // 2) quando è disabilitato deve apparire la scritta Loading (Accendi loading...)
-  // 3) dopo 2 secondi deve tornare nella condizione precedente (solo Accendi)
-
 
 }
