ee868c8d41c4d2f8c49ba784b0b97f38d7b14e4d
Rvbino <v.grasso@itsrizzoli.it>
2022-09-19 15:11:05 +0200

    lezione4-prompt

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
+  titolo = "Mio titolo"
+  disabilitato = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onPremuto(){
+    prompt("nuovo titolo")
   }
 
 }
