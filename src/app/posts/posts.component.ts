ab2daf3a33c68db3fc02a776c7a28dbbfa334c28
Riccardo <c.lin@itsrizzoli.it>
2022-09-19 16:29:42 +0200

    lezione4_esercizio5

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../dati/posts.data';
 
 @Component({
   selector: 'app-posts',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
+  posts:Post[]=[]
   titolo = "Mio titolo"
   disabilitato = false
   testoStatoBottone = ""
@@ -17,29 +19,23 @@ export class PostsComponent implements OnInit {
   }
 
   onPremuto() {
-    const t = prompt("Inserisci titolo", this.titolo)
-    if (t){
-      this.titolo = t
+    const nomePost = prompt("Inserisci nuove post")
+    if (nomePost){
+
+      const newPost:Post={
+        titolo:nomePost,
+        testo:"Test",
+        commenti:[]
+      }
+      this.posts.push(newPost)
+
     } else {
       this.titolo = ""
     }
   }
 
-  onLoading() {
-    this.disabilitato = true
-    this.testoStatoBottone = "loading..."
-
-    setTimeout(()=> {
-      this.disabilitato = false
-      this.testoStatoBottone = ""
-    },2000)
+  getPostsString(){
+    return JSON.stringify(this.posts)
   }
-
-
-
-  // 1) bottone Accendi: premendo si deve disabilitare
-  // 2) quando è disabilitato deve apparire la scritta Loading (Accendi loading...)
-  // 3) dopo 2 secondi deve tornare nella condizione precedente (solo Accendi)
-
-
+  
 }
