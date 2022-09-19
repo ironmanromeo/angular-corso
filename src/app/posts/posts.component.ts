4b55871fa91e32ff68858f84a4a4f21794e774e4
itscailor <l.cairo@itsrizzoli.it>
2022-09-19 16:31:15 +0200

    postsdata

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -22,29 +22,17 @@ export class PostsComponent implements OnInit {
   }
 
   onPremuto() {
-    const t = prompt("Inserisci titolo", this.titolo)
-    if (t){
-      this.titolo = t
-    } else {
-      this.titolo = ""
-    }
-  }
-
-  onLoading() {
-    this.disabilitato = true
-    this.testoStatoBottone = "loading..."
-
-    setTimeout(()=> {
-      this.disabilitato = false
-      this.testoStatoBottone = ""
-    },2000)
-  }
-
-
-
-  // 1) bottone Accendi: premendo si deve disabilitare
-  // 2) quando è disabilitato deve apparire la scritta Loading (Accendi loading...)
-  // 3) dopo 2 secondi deve tornare nella condizione precedente (solo Accendi)
-
-
+    const nomePost = prompt("Inserisci un nuovo post")
+    if (nomePost){
+      const newPost:Post = {
+        titolo:nomePost,
+        testo:"Test",
+        commenti:[]
+      }
+      this.posts.push(newPost)
+    } 
 }
+getPostsString(){
+  return JSON.stringify(this.posts)
+}
+  }
