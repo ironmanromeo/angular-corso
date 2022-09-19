f945f5209905fc9c87c4e30662bf2b344f1cd7b7
Hicham <99076537+hicham-tijani@users.noreply.github.com>
2022-09-19 11:00:36 +0200

    lezione4

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,25 +1,58 @@
-//import { type } from "os"
-
-
-type Tipo1 = {
-    nome: string;
-    cognome: string;
-    eta: number
-  }
-  
-  type Tipo2 = {
-    altezza: number;
-    occhiali: boolean
-  }
-  
-  type PersonaTipo = Tipo1 & Tipo2
-  
-  const persona: PersonaTipo = {
+const persona = {
     nome: "Renzo",
     cognome: "Tramaglino",
-    eta: 40,
-    altezza: 1.70,
-    occhiali: false
+    eta: 20
   }
-  
-  console.log(persona)
+
+  const nomi:string[] = []
+  const nomi2 = new Array<string>()
+
+
+  //classe
+  class Persona {
+    nome: string
+    cognome: string
+    eta: number
+
+    //costruttore
+    constructor(n:string,c:string,e:number){
+        this.nome=n
+        this.cognome=c
+        this.eta=e
+      }
+
+      //funzione
+      nomeCognome(){
+        return this.nome + this.cognome
+      }
+
+  }
+
+const persona1 = new Persona("Renzo","Tramaglino",20)
+console.log(persona1.nomeCognome);
+      
+class MyStorage<T>{
+      item:T[] = []
+      
+      addItem(item:T){
+        this.items.push(item)
+      }
+      removeItem(){
+        this.items.pop()
+      }
+
+      getItems(){
+        return [...this.item]
+      }
+}
+
+const myStorage = new MyStorage<string|number>();
+myStorage.addItem("Pollo")
+
+const lista1 = myStorage.getItems()
+const nome1 = lista1[0]
+const lengthnome1 = nome1.length
+
+console.log(myStorage.getItems())
+
+
