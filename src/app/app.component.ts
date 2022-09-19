e221ef5809e309e68aac0dd037a5809769d96fe0
Riccardo <c.lin@itsrizzoli.it>
2022-09-19 14:31:33 +0200

    lezione 4 - inizio componenti Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titolo="Titolo pagina"
-  altezza = 24
-  disabilitato= true
-  divText="HELLO"
-
-  constructor(){
-    
-    setInterval(()=>{
-      this.disabilitato=!this.disabilitato
-    },1000)
-
-    this.disabilitato=false
-
-  }
+  title="angular-corso"
 }
