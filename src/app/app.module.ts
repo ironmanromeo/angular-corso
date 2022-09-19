7f280faee7167c5bae223bdea7cd6670faa7ec8c
Romeo Mariani <romeo.mariani@valxer.it>
2022-09-19 11:54:17 +0200

    lezione 4 - reset progetto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PersoneComponent } from '../persone/persone.component'
 
 import './training_javascript'
 import './training_typescript'
@@ -11,7 +10,6 @@ import './training_typescript'
 @NgModule({
   declarations: [
     AppComponent,
-    PersoneComponent
   ],
   imports: [
     BrowserModule,
