52ea9809d78482d47ead858329d2781667aa03fb
Rvbino <v.grasso@itsrizzoli.it>
2022-09-19 12:02:53 +0200

    lezione3-obj

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,16 +1,24 @@
-const obj ={
-    name: "R",
-    cognome: "L"
+function merge<T,U>(v1:T, v2:U){
+    return Object.assign(v1,v2)
+    //return {...v1,...v2}
 }
 
-const obj2={
+const obj1 = {
+    nome:"Renzo",
+    cognome:"Tramaglino"
+}
+
+const obj2 = {
     eta:20
 }
 
-function merge<T,U>(v1:T,v2:U){
-    const union = Object.assign(v1,v2)
-    return union
+const obj3 = {
+    altezza:1.74,
+    peso:"troppo"
 }
 
-const merged=merge(obj,obj2)
-console.log(merged.eta)
+const merged = merge(obj1,obj2)
+const merged2 = merge(obj1,obj3)
+//const merged3 = merge(merged,23)
+
+//console.log(merged3)
