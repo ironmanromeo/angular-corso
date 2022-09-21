94b9208543c353c141ee7b213296d7e32a5fd000
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-09-21 17:42:44 +0200

    lezione 5 - accordion

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, } from '@angular/core';
+import { Component, Input, } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -7,9 +7,15 @@ import { Component, } from '@angular/core';
 })
 export class AccordionComponent {
 
+
+  @Input() titolo = "Mio titolo"
+
   aperto = true
 
-  constructor() { }
+  constructor() { 
+
+    
+  }
 
   onPremutoTitolo() {
     this.aperto = !this.aperto
