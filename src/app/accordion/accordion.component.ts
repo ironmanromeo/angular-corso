f97a0bce894616b918489ec9d42ce2b366c13a56
Paolo D'Alessandro <paolodalex96@gmail.com>
2022-09-21 17:06:12 +0200

    lezione 5 - esercizio accordion

---
+++ src/app/accordion/accordion.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.css']
+})
+export class AccordionComponent {
+  
+  @Input() title = ""
+  @Input() text = ""
+
+  status = true
+
+  constructor() { }
+
+  changeStatus() {
+    this.status = !this.status
+    
+  }
+
+
+}
