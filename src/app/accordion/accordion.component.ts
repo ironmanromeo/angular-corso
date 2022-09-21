aedc999e7e7263f44bc62afa17382471432bd5b8
itscailor <l.cairo@itsrizzoli.it>
2022-09-21 17:42:11 +0200

    accordion

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent {
+  
+  @Input() titolo= "Mio Titolo"
 
   aperto = true
 
