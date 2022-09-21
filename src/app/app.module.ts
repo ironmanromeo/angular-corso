1ad3d04e8569c3d81dae8ba9f018f775ff1d3cbb
andre <d.castronunez@itsrizzoli.it>
2022-09-21 15:35:07 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component'
 import { PostComponent } from './post/post.component';
-import { FormComponent } from './form/form.component';
 
 import './training_javascript'
 import './training_typescript';
-import { PostComponent } from './post/post.component';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
 
