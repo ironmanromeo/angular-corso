dd32dc78f45dc91da3466c22afc08b77c89dd331
hCri02 <c.hanu@itsrizzoli.it>
2022-09-21 14:46:13 +0200

    Double Input

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  nome = "Pippo"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onInput(e:Event){
+    const el = e.target as HTMLInputElement
+    this.nome = el.value
+  }
+
+}
