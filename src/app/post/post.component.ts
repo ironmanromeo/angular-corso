43e8d258b34c9f2dd911dd2ddc1a80e43d1bad23
Hicham <99076537+hicham-tijani@users.noreply.github.com>
2022-09-21 11:15:56 +0200

    Lezione5_random

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-import { Component, Input } from "@angular/core";
 
-@Component({
-  selector: "app-post",
-  templateUrl: "./post.component.html",
-  styleUrls: ["./post.component.css"]
-})
-export class PostComponent {
-  @Input("ti") titolo = "titolo post";
-
-  constructor(){}
-=======
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -23,6 +11,6 @@ export class PostComponent {
   @Input("ti") titolo = "Mio titolo"
 
   constructor() { }
->>>>>>> origin/main
+
 
 }
