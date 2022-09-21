99eb02ac13dfb764527f57cfd71c1d8f1616af49
Rivi02 <r.verderio@itsrizzoli.it>
2022-09-21 14:40:28 +0200

    lezione 5 es bho

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+  nome= "pippo"
+
+  constructor() { }
+
+  onInput(e:Event){
+    const el = e.target as HTMLInputElement 
+    this.nome = el.value
+  }
+}
