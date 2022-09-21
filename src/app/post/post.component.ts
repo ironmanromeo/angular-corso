ad6fe5527d554849f6b8ff2ef8e17c4183825c6e
andre <d.castronunez@itsrizzoli.it>
2022-09-21 12:39:03 +0200

    EventEmitter

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class PostComponent {
 
   @Input("ti") titolo = "Mio titolo"
+  @Output() generaEvento = new EventEmitter<string>()
 
   constructor() { }
 
