7ae95280b2d71a2ae1e7935c512a1d72654afa99
ivanluc2002 <103107740+ivanluc2002@users.noreply.github.com>
2022-09-21 17:27:43 +0200

    accordion appear disappear

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent implements OnInit {
+  aperto=true
+  @Input("t") titolo="ZAO"
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+scompare(){
+  this.aperto=!this.aperto
+}
 }
