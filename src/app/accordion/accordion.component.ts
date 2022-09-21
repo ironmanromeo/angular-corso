8be5f8e61402b222c70102966feacceb726cbe19
riccardo de gasperi <riccardo.degasperi@itsrizzoli.it>
2022-09-21 17:31:31 +0200

    Lezione5 esercizio5

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
 
+  @Input("titoloAccordion") titolo:string = "Mio accordion"
+
+  stato = true
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAccordionClick() {
+    this.stato = !this.stato
   }
 
 }
