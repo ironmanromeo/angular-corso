c7fd006258fc89d8d5add7ca7d07cf4fca9d5f0a
itscailor <l.cairo@itsrizzoli.it>
2022-09-30 15:29:11 +0200

    accordion

---
+++ src/app/subordinato/subordinato.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subordinato',
+  templateUrl: './subordinato.component.html',
+  styleUrls: ['./subordinato.component.css']
+})
+export class SubordinatoComponent implements OnInit {
+
+  @Input() testo = ""
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    console.log("costruttore",this.testo)
+  }
+
+}
