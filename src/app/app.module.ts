6f0ca2c153076de37efb93e69139823aa705764c
Gabriele <g.zulini@itsrizzoli.it>
2022-09-30 15:29:23 +0200

    lezione6-ngOnInit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { FormComponent } from './form/form.component';
 import './training_javascript'
 import './training_typescript';
 import { AccordionComponent } from './accordion/accordion.component';
+import { PrincipaleComponent } from './principale/principale.component';
+import { SubordinatoComponent } from './subordinato/subordinato.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,8 @@ import { AccordionComponent } from './accordion/accordion.component';
     PostComponent,
     FormComponent,
     AccordionComponent,
+    PrincipaleComponent,
+    SubordinatoComponent,
   ],
   imports: [
     BrowserModule,
