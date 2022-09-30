170211ef6516b0eb4dea12c9198d52ac5ff244f4
Paolo D'Alessandro <paolo@MacBook-Pro-di-Paolo.local>
2022-09-30 16:47:46 +0200

    lezione6 - Host Listener

--- src/app/applica-colore.directive.ts
+++ src/app/applica-colore.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostBinding, OnInit, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appApplicaColore]'
@@ -7,12 +7,23 @@ export class ApplicaColoreDirective implements OnInit{
 
   @HostBinding("style.backgroundColor") backgroundColor?:string
 
+
   constructor(public elementRef: ElementRef) {
 
   }
 
+  @HostListener("mouseenter")
+  onMouseEnter() {
+    this.backgroundColor = "yellow"
+  }
+
+  @HostListener("mouseleave")
+  onMouseLeave() {
+    this.backgroundColor = "transparent"
+  }
+
   ngOnInit(): void {
-    this.backgroundColor = "red"
+    //this.backgroundColor = "red"
   }
 
 }
