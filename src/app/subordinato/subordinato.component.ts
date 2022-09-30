1daf65512d5fb60cf511ac3c2090dabdfb3d62ca
Gabriele <g.zulini@itsrizzoli.it>
2022-09-30 15:58:26 +0200

    lezione6

--- src/app/subordinato/subordinato.component.ts
+++ src/app/subordinato/subordinato.component.ts
@@ -1,17 +1,23 @@
-import { Component,Input, OnInit } from '@angular/core';
+import { Component,Input, OnInit, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-subordinato',
   templateUrl: './subordinato.component.html',
   styleUrls: ['./subordinato.component.css']
 })
-export class SubordinatoComponent implements OnInit {
+export class SubordinatoComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() testo = ""
 
   constructor() {
     console.log(this.testo);
     
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
   }
 
   ngOnInit(): void {
