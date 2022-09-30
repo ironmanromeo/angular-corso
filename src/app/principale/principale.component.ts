ae55d73db1a3c0518b9e1a3a9ebdae83e75e5bf7
Riccardo <c.lin@itsrizzoli.it>
2022-09-30 15:57:07 +0200

    lezione6_nascondi subordinato

--- src/app/principale/principale.component.ts
+++ src/app/principale/principale.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-principale',
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipaleComponent implements OnInit {
 
+
+  @Input() mostraSubordinato:boolean=false
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cambiaStato(){
+    this.mostraSubordinato=!this.mostraSubordinato
+    console.log(this.mostraSubordinato)
+  }
 
 
 }
