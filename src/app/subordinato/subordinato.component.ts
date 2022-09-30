b2fb03daf87f766d4d05b4262e67b8cb41a6caa1
Rvbino <v.grasso@itsrizzoli.it>
2022-09-30 15:58:31 +0200

    lezione6-ondestroy

--- src/app/subordinato/subordinato.component.ts
+++ src/app/subordinato/subordinato.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-subordinato',
   templateUrl: './subordinato.component.html',
   styleUrls: ['./subordinato.component.css']
 })
-export class SubordinatoComponent implements OnInit {
+export class SubordinatoComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() testo = "qualcosa"
 
   constructor() { 
     console.log(this.testo)
    }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("OnChanges",this.testo)
+  }
+  ngOnDestroy(): void {
+    console.log("OnDestroy",this.testo)
+  }
 
   ngOnInit(): void {
     console.log(this.testo)
