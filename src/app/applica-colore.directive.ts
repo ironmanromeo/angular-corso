f8a003fbd135412d5d1f941238c1af75ec917997
Simone Marcandalli <MY_NAME@example.com>
2022-09-30 17:09:52 +0200

    coloreoffon

--- src/app/applica-colore.directive.ts
+++ src/app/applica-colore.directive.ts
@@ -1,12 +1,27 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appApplicaColore]'
 })
 export class ApplicaColoreDirective {
-
+  
+@Input() colorOff?:string
+@Input() colorOn?:string
+  @HostBinding("style.backgroundColor") backgroundcolor?:string
   constructor(public elementRef: ElementRef, public renderer: Renderer2) {
+    this.backgroundcolor=this.colorOff
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green')
+  }
+  @HostListener("mouseenter")
+  onMouseEnter(){
+    this.backgroundcolor=this.colorOn
+  }
+  @HostListener("mouseleave")
+  onMouseLeave(){
+    this.backgroundcolor=this.colorOff
+  }
+  ngOnInit():void{
+
   }
 
 }
