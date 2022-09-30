0f2084008016c80791e0bbf4af484f1ec4f66947
ivanluc2002 <103107740+ivanluc2002@users.noreply.github.com>
2022-09-30 16:33:42 +0200

    direttiva renderer2 lezione 6

---
+++ src/app/applica-colore.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appApplicaColore]'
+})
+export class ApplicaColoreDirective {
+
+  constructor( public renderer:Renderer2, public elementRef:ElementRef) {
+    this.renderer .setStyle(this.elementRef.nativeElement, "backgroundColor", "red")
+   }
+
+}
