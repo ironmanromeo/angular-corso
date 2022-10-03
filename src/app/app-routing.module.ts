02e7e4e3110be4e9d0a5fdc9e503a0707e2ba192
andre <d.castronunez@itsrizzoli.it>
2022-10-03 15:14:12 +0200

    routerActiveOption e routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { ProdottiComponent } from './prodotti/prodotti.component';
 
 const routes: Routes = [
   {path:"", component:HomeComponent},
-  {path:"prodotti", component:ProdottiComponent}
+  {path:"prodotti", component:ProdottiComponent},
+  {path:"login", component:LoginComponent},
 ];
 
 @NgModule({
