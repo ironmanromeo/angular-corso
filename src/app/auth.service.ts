5a1c0299c3e6c17b57b4d55b1b5cf4b816ad199e
andre <d.castronunez@itsrizzoli.it>
2022-10-03 17:57:34 +0200

    sistema autenticazione iniziato

---
+++ src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLogged = false
+
+  constructor() { }
+
+  logIn (){
+    this.isLogged = true
+  }
+
+  logOut(){
+    this.isLogged = false
+  }
+
+  isAutenticated(){
+    const promise = new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        resolve (this.isLogged)
+      }, 500);
+    })
+    return promise
+  }
+
+  
+}
