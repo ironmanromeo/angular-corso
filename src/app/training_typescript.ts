8f34f0c12e23fdc5a360cba07f4fd554a54394ec
Paolo D'Alessandro <paolo@MacBook-Pro-di-Paolo.local>
2022-10-03 15:57:49 +0200

    lezione7 - router4 metodi accessori get e set

--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,13 +1,28 @@
-type persona = {
-  nome:string,
-  cognome:string,
-  eta:Number,
-  altezza?:Number
+class Datastore {
+  private _dati :string = ""
+
+  get dati() {
+    console.log("sto leggendo i dati");
+
+    return this._dati
+  }
+
+  set dati(dati :string) {
+    if (dati !== "pippo") {
+      console.log("sto settando i dati");
+      this._dati = dati
+    } else {
+      console.log("Errore!");
+
+    }
+
+  }
+
 }
 
-const persona1 = {
-  nome:"Mario",
-  cognome:"Rossi",
-  eta:20,
-  altezza:1.85
-}
+const ds = new Datastore()
+
+ds.dati = "pippo"
+
+console.log(ds.dati);
+
