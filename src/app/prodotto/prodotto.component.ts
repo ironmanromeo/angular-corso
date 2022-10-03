ee68504795fece39ae6ff76e48208ad79442dae0
riccardo de gasperi <riccardo.degasperi@itsrizzoli.it>
2022-10-03 17:13:54 +0200

    Lezione7 router5

--- src/app/prodotto/prodotto.component.ts
+++ src/app/prodotto/prodotto.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Prodotto } from '../dati/prodotto.data';
+import { ProdottoService } from '../prodotto.service';
 
 @Component({
   selector: 'app-prodotto',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prodotto.component.css']
 })
 export class ProdottoComponent implements OnInit {
+  prodotto?:Prodotto
+  subscription?:Subscription
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private prodottoService:ProdottoService, private router:Router) {
+    console.log(route)
+    const { slug } = route?.snapshot?.params ?? {}
+    this.prodotto = prodottoService.cercaProdotto(slug)
+
+    this.subscription = route?.params?.subscribe(params => {
+      const { slug } = params ?? {}
+      this.prodotto = prodottoService.cercaProdotto(slug)
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe()
+  }
+
+  vaiAProdotto2() {
+    this.router.navigate(["/prodotti/prod2"])
   }
 
 }
--- src/app/training_typescript.ts
+++ src/app/training_typescript.ts
@@ -1,28 +1,28 @@
-class Datastore {
-  private _dati :string = ""
+// class Datastore {
+//   private _dati :string = ""
 
-  get dati() {
-    console.log("sto leggendo i dati");
+//   get dati() {
+//     console.log("sto leggendo i dati");
 
-    return this._dati
-  }
+//     return this._dati
+//   }
 
-  set dati(dati :string) {
-    if (dati !== "pippo") {
-      console.log("sto settando i dati");
-      this._dati = dati
-    } else {
-      console.log("Errore!");
+//   set dati(dati :string) {
+//     if (dati !== "pippo") {
+//       console.log("sto settando i dati");
+//       this._dati = dati
+//     } else {
+//       console.log("Errore!");
 
-    }
+//     }
 
-  }
+//   }
 
-}
+// }
 
-const ds = new Datastore()
+// const ds = new Datastore()
 
-ds.dati = "pippo"
+// ds.dati = "pippo"
 
-console.log(ds.dati);
+// console.log(ds.dati);
 
