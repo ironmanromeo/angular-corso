47c15924842213d65909f1d8946e9a5a529b09be
Gabriele <g.zulini@itsrizzoli.it>
2022-10-03 16:33:25 +0200

    lezione7

--- src/app/prodotto/prodotto.component.ts
+++ src/app/prodotto/prodotto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Prodotto } from '../dati/prodotto.data';
 
 @Component({
   selector: 'app-prodotto',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdottoComponent implements OnInit {
 
-  constructor() { }
+  slug :string = ""
+  prodotto?: Prodotto
+
+  constructor(private route: ActivatedRoute) {
+    console.log(route)
+    const {slug:slugTemp} = route?.snapshot?.params ?? {}
+    this.slug = slugTemp
+    
+   }
 
   ngOnInit(): void {
   }
