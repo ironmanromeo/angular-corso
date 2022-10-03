1497893606d59d61dd450574bc5b79ad61e53283
Gabriele <g.zulini@itsrizzoli.it>
2022-10-03 14:46:42 +0200

    lezione7

---
+++ src/app/training_typescript.ts
@@ -0,0 +1,13 @@
+type persone = {
+    nome:string,
+    cognome:string,
+    eta:Number,
+    altezza?:Number
+}
+
+const persona1 = {
+    nome:"Mario",
+    cognome:"Rossi",
+    eta:20,
+    altezza:1.85
+}
