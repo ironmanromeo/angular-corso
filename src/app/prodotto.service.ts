e583a099537e3f3cfe7a5acde0a62838886d3540
riccardo de gasperi <riccardo.degasperi@itsrizzoli.it>
2022-10-07 16:05:11 +0200

    Lezione8 observable3

--- src/app/prodotto.service.ts
+++ src/app/prodotto.service.ts
@@ -25,6 +25,12 @@ export class ProdottoService {
       setInterval(() => {
         subscribe.next(count)
         count++
+        if (count > 10) {
+          subscribe.complete()
+        }
+        if (count > 7) {
+          subscribe.error(new Error("Stramegaiper errore"))
+        }
       }, 1000)
     })
   }
