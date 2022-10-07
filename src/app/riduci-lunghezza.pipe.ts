28862bbf63328533417e5f74fb15794ecf9a40d4
Paolo Dalex <paolodalex96@gmail.com>
2022-10-07 14:35:48 +0200

    lezione8 - pipe personale

---
+++ src/app/riduci-lunghezza.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'riduciLunghezza'
+})
+export class RiduciLunghezzaPipe implements PipeTransform {
+
+  transform(value: string, limite: number = 10): string {
+    if (value.length > limite) {
+      return value.substring(0, limite)
+    }
+    return value;
+  }
+
+}
