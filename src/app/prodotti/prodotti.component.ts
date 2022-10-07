c0478ed19761c8fc2e44933705e411c2f82813f6
ivanluc2002 <103107740+ivanluc2002@users.noreply.github.com>
2022-10-07 15:19:17 +0200

    subscribe lezione 8

--- src/app/prodotti/prodotti.component.ts
+++ src/app/prodotti/prodotti.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { count, interval, Subscription } from 'rxjs';
 import { Prodotto } from '../dati/prodotto.data';
 import { ProdottoService } from '../prodotto.service';
 
@@ -8,19 +9,22 @@ import { ProdottoService } from '../prodotto.service';
   templateUrl: './prodotti.component.html',
   styleUrls: ['./prodotti.component.css']
 })
-export class ProdottiComponent implements OnInit {
-
-  testoAsync = new Promise((resolve, reject) => {
-    setTimeout( () => {
-      resolve("Pippo")
-    }, 2000)
-  })
+export class ProdottiComponent implements OnInit, OnDestroy {
+  private intervalSubscription?:Subscription 
   ricerca = ""
   prodotti :Prodotto[] = []
   constructor(private router: Router,private prodottoService :ProdottoService) {
     this.prodotti = this.prodottoService.prodotti
+    
    }
+  ngOnDestroy(): void {
+    this.intervalSubscription?.unsubscribe()
+  }
   ngOnInit(): void {
+    this.intervalSubscription= interval (1000).subscribe(count=>{
+      console.log(count)
+    }
+    )
   }
 
 
