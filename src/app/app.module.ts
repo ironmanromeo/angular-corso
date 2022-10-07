3a7745c7d00294d78e25aa36c678df09d36ea3c9
DaniAvolio <avoliodaniele9@gmail.com>
2022-10-07 17:40:33 +0200

    fix: my version of checkout2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component'
@@ -49,7 +48,8 @@ import { Checkout2Component } from './checkout2/checkout2.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/checkout2/checkout2.component.ts
+++ src/app/checkout2/checkout2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout2',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout2.component.css']
 })
 export class Checkout2Component implements OnInit {
+  checkoutForm: FormGroup
 
-  constructor() { }
+  constructor() {
+    this.checkoutForm = new FormGroup({
+      "firstname" : new FormControl("pippo", Validators.required),
+      "lastname" : new FormControl(null,Validators.required),
+      "username" : new FormControl(null,Validators.required),
+      "email" : new FormControl(null, [Validators.required, Validators.email]),
+
+    })
+  }
 
   ngOnInit(): void {
+  }
+  onSubmit(){
+    
   }
 
 }
