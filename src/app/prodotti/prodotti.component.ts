9a9cd670b183289483b65070e200b9af82adac50
ivanluc2002 <103107740+ivanluc2002@users.noreply.github.com>
2022-10-07 15:05:01 +0200

    pipe async lezione 8

--- src/app/prodotti/prodotti.component.ts
+++ src/app/prodotti/prodotti.component.ts
@@ -9,7 +9,13 @@ import { ProdottoService } from '../prodotto.service';
   styleUrls: ['./prodotti.component.css']
 })
 export class ProdottiComponent implements OnInit {
-
+  testoAsync= new Promise((resolve, reject)=>
+  {
+    setTimeout (()=>{
+    resolve("pippo")
+  },2000)
+   
+  })
   ricerca = ""
   prodotti :Prodotto[] = []
   constructor(private router: Router,private prodottoService :ProdottoService) {
