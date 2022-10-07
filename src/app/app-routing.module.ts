eb82e7bb30819b4c7056e4e75e9d5028508a0476
Riccardo <c.lin@itsrizzoli.it>
2022-10-07 16:37:48 +0200

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
-import { AuthGuardService } from './auth-guard.service';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
