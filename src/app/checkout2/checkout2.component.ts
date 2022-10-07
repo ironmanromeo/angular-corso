de71f773192e8c20eacb5a767d97a32bfaaea804
ivanluc2002 <103107740+ivanluc2002@users.noreply.github.com>
2022-10-07 17:39:53 +0200

    reactive form

--- src/app/checkout2/checkout2.component.ts
+++ src/app/checkout2/checkout2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout2',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout2.component.css']
 })
 export class Checkout2Component implements OnInit {
-
-  constructor() { }
+  checkOutForm: FormGroup
+  constructor() { 
+    this.checkOutForm= new FormGroup({
+    "firstName" : new FormControl("pippo",Validators.required),
+    "lastName"  : new FormControl(null,Validators.required ),
+    "username"  : new FormControl(null,Validators.required),
+    "email" : new FormControl(null,[Validators.required, Validators.email])
+    })
+  }
 
   ngOnInit(): void {
   }
