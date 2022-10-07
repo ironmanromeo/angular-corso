7ea2375cf3b6fdf1d383a08396c474054f6beee5
Marco Stefani <m.stefani@itsrizzoli.it>
2022-10-07 15:22:54 +0200

    feat: pipes

--- src/app/prodotto/prodotto.component.ts
+++ src/app/prodotto/prodotto.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Prodotto } from '../dati/prodotto.data';
+import { ProdottoService } from '../prodotto.service';
 
 @Component({
   selector: 'app-prodotto',
   templateUrl: './prodotto.component.html',
   styleUrls: ['./prodotto.component.css']
 })
-export class ProdottoComponent implements OnInit {
+export class ProdottoComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  prodotto?: Prodotto
+  subscription?: Subscription
+  
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private service: ProdottoService,
+              private router: Router) {
+      const { slug } = route?.snapshot?.params ?? {}
+      this.prodotto = service.cercaProdotto(slug) 
+      
+      this.subscription = route.params.subscribe(params => {
+        console.log(params);
+        const { slug } = params
+        this.prodotto = service.cercaProdotto(slug)
+      })
+    }
+  ngOnInit(): void {    
+  }
+  
+  ngOnDestroy(): void {
+    console.log("OnDestoy", this.prodotto);
+    this.subscription?.unsubscribe()
+  }
+
+  vaiProd2(){
+    this.router.navigate(["/prodotti/prod2"])
   }
 
 }
