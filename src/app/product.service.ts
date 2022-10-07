71aa64a0ffb7d3efb34c99b33e00600a1a1c8d25
ivanluc2002 <103107740+ivanluc2002@users.noreply.github.com>
2022-10-07 16:03:17 +0200

    lezione 8 non va observable

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
+import { observable } from 'rxjs';
 import { Prodotto } from './dati/prodotto.data'
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private _prodotti :Prodotto[]=[
-    {codice:"001",nome:"prodotto1",prezzo:100,slug:"prod1",foto:""},
-    {codice:"002",nome:"prodotto2",prezzo:200,slug:"prod2",foto:""},
-    {codice:"003",nome:"prodotto3",prezzo:300,slug:"prod3",foto:""},
-    {codice:"004",nome:"prodotto1",prezzo:100,slug:"prod4",foto:""},
-    {codice:"005",nome:"prodotto2",prezzo:200,slug:"prod5",foto:""},
+    {codice:"001",nome:"prodotto1",categoria:1,prezzo:100,slug:"prod1",foto:""},
+    {codice:"002",nome:"prodotto2",categoria:2,prezzo:200,slug:"prod2",foto:""},
+    {codice:"003",nome:"prodotto3",categoria:3,prezzo:300,slug:"prod3",foto:""},
+    {codice:"004",nome:"prodotto1",categoria:4,prezzo:100,slug:"prod4",foto:""},
+    {codice:"005",nome:"prodotto2",categoria:5,prezzo:200,slug:"prod5",foto:""}
     
   ]
+  public observable:Observable< number >
+  constructor() { 
+    
+    this.observable = new observable (subscribe =>{
+      let count=0
+      setInterval(()=>{
+       
+        subscribe.next(count)
+        count++
+        if (count>=5){
+          subscribe.complete()
+        }
+      })
+    })
+  }
   private _carrello :Prodotto[]=[]
-
+ 
+  
   aggiungiCarrello(prodotto:Prodotto){
     this._carrello.push(prodotto)
   }
@@ -26,5 +43,5 @@ export class ProductService {
   get carrello(){
     return [...this._carrello]
   }
-  constructor() { }
+  
 }
