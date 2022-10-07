ee8cd57df8bd51b6e0c46d0bbda4bb41529a5673
riccardo de gasperi <riccardo.degasperi@itsrizzoli.it>
2022-10-07 15:04:33 +0200

    Lezione8 pipe3

--- src/app/prodotti/prodotti.component.ts
+++ src/app/prodotti/prodotti.component.ts
@@ -10,6 +10,12 @@ import { ProdottoService } from '../prodotto.service';
 })
 export class ProdottiComponent implements OnInit {
 
+  testoAsync = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Pippo")
+    }, 2000)
+  })
+
   ricerca = ""
   prodotti :Prodotto[] = []
   constructor(private router: Router,private prodottoService :ProdottoService) {
