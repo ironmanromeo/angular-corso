ab5263b77e07c4bf3f915064a6afb3ecac9eb228
itscailor <l.cairo@itsrizzoli.it>
2022-10-07 14:36:02 +0200

    lezione8-ngonit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  nome = "Pippo Pluto e Paperino"
+  miaData = new Date
   isLogged = false
 
   //testoBottone = "Non loggato"
